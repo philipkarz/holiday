759a91ac50e9bdbe75fae0148d56f263ab87ca83
Philip Karz <phillipkarz@gmail.com>
2017-10-06 09:35:48 -0700

    fix2

--- routes/holidays.js
+++ routes/holidays.js
@@ -1,17 +1,9 @@
 const
 express = require('express'),
 holidaysRouter = new express.Router(),
-//..up 2 directories
+
 holidaysCtrl = require('../controllers/videos.js')
 
-
-// videosRouter.route('/')
-// .get(videosCtrl.index)
-// .get(videosCtrl.create)
-
-// videosRouter.route('/:id')
-// .get(videosCtrl.show)
-// .get(videosCtrl.destroy)
 
 videosRouter.get('/', holidaysCtrl.index)
 
