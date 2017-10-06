2dbc240aa42d91ac5b609c5c743538a577ac1115
Charles Anderson <charlesandreanderson22@gmail.com>
2017-10-06 09:21:14 -0700

    update

--- models/holiday.js
+++ models/holiday.js
@@ -1,9 +1,9 @@
 const
   mongoose = require('mongoose')
   holidaySchema = new mongoose.Schema({
-      name: String,
-      date: Date,
-      religious: Boolean
+      name: {type: String, required: true},
+      date: {type: Date, required: true},
+      religious: {type: Boolean}
   })
 
   
